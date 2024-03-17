8e08161d636868eb57023af2759c6b52f109c601
ashudip <ashudeepdev@gmail.com>
2024-03-17 16:32:40 +0530

    pushing from eclipse

--- src/test/java/practise/TestClass.java
+++ src/test/java/practise/TestClass.java
@@ -3,5 +3,7 @@ package practise;
 public class TestClass {
     public static void main(String[] args) {
         System.out.println("this is commit from intellij");
+        System.out.println("this is a commit from exlipse to test intellij pull");
+        System.out.println("checkign new one");
     }
 }
